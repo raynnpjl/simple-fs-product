bc25a7d157fe59b6eb681985294826d50639e68e
raynnpjl <vqckmc@gmail.com>
2025-04-24 11:21:31 +0800

    routes

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -3,4 +3,9 @@ const productController = require('../controllers/productController');
 
 const router = express.Router();
 
-router.get('/',)
+router.get('/', productController.listProducts);
+router.post('/', productController.createProduct);
+router.put('/:product_id', productController.updateProduct);
+router.delete('/:product_id', productController.deleteProduct);
+
+module.exports = router;
